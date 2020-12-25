8d8c9936107cdb8bb675d89976de458aea0ef2ac
WangXi@Desktop <xicser@126.com>
2020-12-25 17:08:47 +0800

    add cpp_const_pointer.

---
+++ cpp_const_pointer/main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+void test1(void);
+void test2(void);
+void test3(void);
+void test4(const int * const p);
+int * const test5(void);
+const int *test6(void);
+
+int main(void)
+{
+    int a = 0;
+
+    test1();
+    test2();
+    test3();
+    test4(&a);
+    int *p = test5();
+    cout << *p << endl;
+    const int *q = test6();
+    cout << *q << endl;
+
+
+    int * const r = &a; //定义的时候，必须初始化
+//    r = 0;   //报错
+
+    return 0;
+}
+
+void test1(void)
+{
+    int a = 10;
+    const int *p = &a; //不可以用*p改变a的值
+}
+
+void test2(void)
+{
+    int a = 90;
+    int * const p = &a; //指针变量p的值不可以修改
+}
+
+void test3(void)
+{
+    int a = 80;
+    const int* const p = &a; //不可以用*p改变a的值, 同时指针变量p的值也不可以修改
+}
+
+void test4(const int * const p)
+{
+    //*p = 8;
+}
+
+int a = 999;
+int * const test5(void)
+{
+    return &a;
+}
+
+const int *test6(void)
+{
+    return &a;
+}
