989dc7766621a50daa614e51af62647720a80ed3
WangXi@College <xicser@126.com>
2021-08-30 22:07:02 +0800

    add cpp_inline.

---
+++ cpp_inline/inline.h
@@ -0,0 +1,11 @@
+#ifndef __INLINE_H__
+#define __INLINE_H__
+
+inline int add(int a, int b)
+{
+    return a + b;
+}
+
+void inlineTest(void);
+
+#endif // __INLINE_H__
---
+++ cpp_inline/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "inline.h"
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    add(1, 3);
+    inlineTest();
+
+    printf("**********************\n");
+
+    return 0;
+}
