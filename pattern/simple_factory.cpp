5024eaeca51f43efbb23c84b941d3bd77cc35f26
WangXi@Laptop <xicser@126.com>
2022-02-27 14:05:09 +0800

    add pattern dir.

---
+++ pattern/simple_factory.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class AbstractFruit {
+public:
+    virtual void showName() = 0;
+};
+
+class Apple : public AbstractFruit {
+public:
+    virtual void showName() {
+
+    }
+
+
+};
+
+class Banana : public AbstractFruit {
+public:
+    virtual void showName() {
+
+    }
+
+
+};
+
+class Pear : public AbstractFruit {
+public:
+    virtual void showName() {
+
+    }
+
+
+};
+
+class FruitFactory {
+
+
+
+};
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
