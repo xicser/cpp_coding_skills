f60d79c72077911811a1c9f98d2cfd68d4ee6d0c
WangXi@College <xicser@126.com>
2022-05-09 11:43:15 +0800

    add 3.agent, 4.decorator.

---
+++ pattern/3.agent/3.agent.cpp
@@ -0,0 +1,71 @@
+﻿#define _CRT_SECRUE_NO_WARINGS
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+/* 共有接口 */
+class AbstractCommonInterface {
+public:
+    virtual void run() = 0;
+};
+
+/* 真正的服务器 */
+class MySystem : public AbstractCommonInterface {
+public:
+    virtual void run() {
+        cout << "系统启动..." << endl;
+    }
+};
+
+/* 
+   服务器的代理
+   必须有要权限验证，不是所有人都能来启动我的启动，提供用户名和密码
+   通过这个MySystemProxy代理，控制对MySystem的访问（提供一种代理来控制对其他对象的访问）
+   MySystem有的行为，MySystemProxy也必须要有。
+*/
+class MySystemProxy : public AbstractCommonInterface {
+public:
+    MySystemProxy(string username, string passwd) {
+        this->username = username;
+        this->passwd = passwd;
+        pSystem = new MySystem();
+    }
+    virtual void run() {
+        if (check() == true) {
+            cout << "验证成功..." << endl;
+        }
+        else {
+            cout << "验证失败..." << endl;
+        }
+    }
+
+    bool check() {
+        if (this->username == "admin" && this->passwd == "admin") {
+            return true;
+        }
+
+        return false;
+    }
+
+    ~MySystemProxy() {
+        if (pSystem != nullptr) {
+            delete pSystem;
+        }
+    }
+
+private:
+    MySystem* pSystem;      //服务器
+    string username;
+    string passwd;
+};
+
+
+int main()
+{
+    MySystemProxy proxy("admin", "admin");
+    proxy.run();
+
+
+}
---
+++ pattern/4.decorator/4.decorator.cpp
@@ -0,0 +1,102 @@
+﻿#define _CRT_SECRUE_NO_WARINGS
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/*
+装饰器模式（包装模式）
+一般情况下用继承实现类的功能拓展
+装饰模式可以动态给一个类增加功能
+
+
+
+*/
+
+/* 抽象英雄 */
+class AbtractHero {
+public:
+    virtual void showStatus() = 0;
+
+public:
+    int mHp;
+    int mMp;
+    int mAt;
+};
+
+/* 具体英雄 */
+class HeroA : public AbtractHero {
+public:
+    HeroA() {
+        mHp = 12;
+        mMp = 23;
+        mAt = 34;
+    }
+
+    virtual void showStatus() {
+        cout << "血量" << mHp << endl;
+        cout << "魔法" << mMp << endl;
+        cout << "攻击" << mAt << endl;
+    }
+};
+
+/* 抽象装备 */
+class AbtractEquipment : public AbtractHero {
+public:
+    AbtractEquipment(AbtractHero* hero) {
+        this->pHero = hero;
+    }
+
+    virtual void showStatus() {
+
+    }
+
+    AbtractHero* pHero;
+};
+
+/* 狂徒 */
+class KuangTuEquipment : public AbtractEquipment {
+public:
+    KuangTuEquipment(AbtractHero* hero) : AbtractEquipment(hero) {
+        
+    }
+
+    void addKuangTu() {
+        this->mHp = this->pHero->mHp;
+        this->mMp = this->pHero->mMp;
+        this->mAt = this->pHero->mAt + 30;
+    }
+};
+
+/* 无尽之刃 */
+class WujinKnifeEquipment : public AbtractEquipment {
+public:
+    WujinKnifeEquipment(AbtractHero* hero) : AbtractEquipment(hero) {
+
+    }
+
+    void addWujinKnife() {
+        this->mHp = this->pHero->mHp;
+        this->mMp = this->pHero->mMp + 99;
+        this->mAt = this->pHero->mAt;
+    }
+};
+
+
+
+int main()
+{
+    AbtractHero* hero = new HeroA();
+    hero->showStatus();
+
+    //给裸奔的英雄穿上衣服
+    //狂徒
+    hero = new KuangTuEquipment(hero);
+    hero->showStatus();
+
+    //无尽之刃
+    hero = new WujinKnifeEquipment(hero);
+    hero->showStatus();
+
+    return 0;
+}
