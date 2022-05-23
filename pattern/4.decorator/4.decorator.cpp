55f6e6f95914d145d219bde080fa8682ca0eacae
WangXi@College <xicser@126.com>
2022-05-23 22:12:29 +0800

    modify decorator.

--- pattern/4.decorator/4.decorator.cpp
+++ pattern/4.decorator/4.decorator.cpp
@@ -6,97 +6,83 @@ using namespace std;
 
 /*
 装饰器模式（包装模式）
-一般情况下用继承实现类的功能拓展
+一般情况下用继承实现类的功能拓展, 为了得到新功能, 每次继承一次都会多一个子类, 这样会导致子类非常多
 装饰模式可以动态给一个类增加功能
-
-
 
 */
 
-/* 抽象英雄 */
-class AbtractHero {
-public:
-    virtual void showStatus() = 0;
 
+/* 抽象饮料 */
+class Beverage {
 public:
-    int mHp;
-    int mMp;
-    int mAt;
+    virtual double cost() = 0;        //计算价钱
 };
 
-/* 具体英雄 */
-class HeroA : public AbtractHero {
+/* 奶茶 */
+class MilkTea : public Beverage {
 public:
-    HeroA() {
-        mHp = 12;
-        mMp = 23;
-        mAt = 34;
-    }
-
-    virtual void showStatus() {
-        cout << "血量" << mHp << endl;
-        cout << "魔法" << mMp << endl;
-        cout << "攻击" << mAt << endl;
+    virtual double cost() {
+        cout << "MilkTea cost 5.5 $" << endl;
+        return 5.5;
     }
 };
 
-/* 抽象装备 */
-class AbtractEquipment : public AbtractHero {
+/* 水果茶 */
+class FruitTea : public Beverage {
 public:
-    AbtractEquipment(AbtractHero* hero) {
-        this->pHero = hero;
-    }
-
-    virtual void showStatus() {
-
-    }
-
-    AbtractHero* pHero;
-};
-
-/* 狂徒 */
-class KuangTuEquipment : public AbtractEquipment {
-public:
-    KuangTuEquipment(AbtractHero* hero) : AbtractEquipment(hero) {
-        
-    }
-
-    void addKuangTu() {
-        this->mHp = this->pHero->mHp;
-        this->mMp = this->pHero->mMp;
-        this->mAt = this->pHero->mAt + 30;
+    virtual double cost() {
+        cout << "FruitTea cost 6.5 $" << endl;
+        return 6.5;
     }
 };
 
-/* 无尽之刃 */
-class WujinKnifeEquipment : public AbtractEquipment {
+/* 佐料(装饰器类) */
+class ToppingDecorator : public Beverage {
 public:
-    WujinKnifeEquipment(AbtractHero* hero) : AbtractEquipment(hero) {
-
-    }
-
-    void addWujinKnife() {
-        this->mHp = this->pHero->mHp;
-        this->mMp = this->pHero->mMp + 99;
-        this->mAt = this->pHero->mAt;
-    }
+    //virtual double cost() = 0;        //计算价钱
 };
 
+/* 加布丁 */
+class Pudding : public ToppingDecorator {
+public:
+    Pudding(Beverage* bverage) {
+        this->bverage = bverage;
+    }
 
+    virtual double cost() {
+        return costPudding + bverage->cost();
+    }
+
+private:
+    int costPudding = 2;  //加补丁要两块钱
+    Beverage* bverage;
+};
+
+/* 加珍珠 */
+class Boba : public ToppingDecorator {
+public:
+    Boba(Beverage* bverage) {
+        this->bverage = bverage;
+    }
+
+    virtual double cost() {
+        return costBoba + bverage->cost();
+    }
+
+private:
+    int costBoba = 3;  //加珍珠要三块钱
+    Beverage* bverage;
+};
 
 int main()
 {
-    AbtractHero* hero = new HeroA();
-    hero->showStatus();
+    Beverage* bverage = new MilkTea();      //先来一杯奶茶
+    bverage = new Boba(bverage);            //加一份珍珠
+    bverage = new Boba(bverage);            //再加一份珍珠
+    bverage = new Pudding(bverage);         //加一份布丁
+    double cost = bverage->cost();          //计算总价
 
-    //给裸奔的英雄穿上衣服
-    //狂徒
-    hero = new KuangTuEquipment(hero);
-    hero->showStatus();
-
-    //无尽之刃
-    hero = new WujinKnifeEquipment(hero);
-    hero->showStatus();
+    cout << cost << endl;
 
     return 0;
 }
